958371bb5cdff103f2cef0f394acfbc855acbb42
deysan <ideysan@gmail.com>
2022-07-22 23:18:44 +0300

    feat: add pagination

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -1,17 +1,42 @@
-import { Button, Center, HStack } from '@chakra-ui/react';
 import React from 'react';
 import { ArrowIcon } from './icons';
+import { Button, Center, HStack } from '@chakra-ui/react';
+import { Params } from '../pages/breeds';
 
-export const Pagination: React.FC = () => {
+interface PaginationProps {
+  params: Params;
+  setParams: React.Dispatch<React.SetStateAction<Params>>;
+}
+
+export const Pagination: React.FC<PaginationProps> = ({
+  params,
+  setParams,
+}) => {
   return (
     <Center>
       <HStack spacing={6}>
-        <Button variant="secondary" leftIcon={<ArrowIcon />} isDisabled>
+        <Button
+          variant="secondary"
+          leftIcon={<ArrowIcon />}
+          isDisabled={params.page === 0}
+          onClick={() => {
+            setParams((prevState) => ({
+              ...prevState,
+              page: params.page - 1,
+            }));
+          }}
+        >
           Prev
         </Button>
         <Button
           variant="secondary"
           rightIcon={<ArrowIcon transform="rotate(180deg)" />}
+          onClick={() => {
+            setParams((prevState) => ({
+              ...prevState,
+              page: params.page + 1,
+            }));
+          }}
         >
           Next
         </Button>
